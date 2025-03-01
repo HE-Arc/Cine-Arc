4e50ee6b7d490cf7bc7c007b3c82f60e71447aa3
Sberthoud <131236793+Sberthoud@users.noreply.github.com>
2025-03-01 15:37:27 +0100

    Revert "add periodic task to fill the db"
    This reverts commit 3b1174fe1132724e0bbfa081171a1fc85e37a060.

--- api/cinearc/celery.py
+++
@@ -1,8 +0,0 @@
-import os
-from celery import Celery
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cinearc.settings')
-
-app = Celery('cinearc')
-app.config_from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
--- api/cinearcapp/apps.py
+++ api/cinearcapp/apps.py
@@ -1,12 +1,6 @@
 from django.apps import AppConfig
-from django.db.models.signals import post_migrate
+
 
 class CinearcappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cinearcapp'
-
-    def ready(self):
-        # Import différé pour éviter l'erreur "Apps aren't loaded yet"
-        from django.db.models.signals import post_migrate
-        from .signals import run_task_schedule 
-        post_migrate.connect(run_task_schedule, sender=self)
